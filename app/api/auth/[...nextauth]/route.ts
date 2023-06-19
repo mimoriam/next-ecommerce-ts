66ba1a719803a79867935ca1cd45fea97ddf58e8
Mimo <mimoriam420@gmail.com>
2023-06-19 17:11:33 +0500

    Commit #6 - Prisma Schema added with NextAuth + GitHub 2/2

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,11 +3,18 @@ import { PrismaClient } from '@prisma/client';
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import { Adapter } from 'next-auth/adapters';
 import { prisma } from '@/utils/prisma-init';
+import GithubProvider from 'next-auth/providers/github';
 
 const handler = NextAuth({
   adapter: PrismaAdapter(prisma) as Adapter,
-  secret: 'secret',
-  providers: [],
+  secret: process.env.NEXTAUTH_SECRET,
+  providers: [
+    // http://localhost:3000/api/auth/signin
+    GithubProvider({
+      clientId: process.env.CLIENT_ID as string,
+      clientSecret: process.env.CLIENT_SECRET as string,
+    }),
+  ],
   events: {},
   callbacks: {},
 });
