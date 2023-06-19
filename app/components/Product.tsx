82745606248717287a065469d23c16119734666e
Mimo <mimoriam420@gmail.com>
2023-06-20 00:56:13 +0500

    Commit #13 - Formatted Price using Intl

--- app/components/Product.tsx
+++ app/components/Product.tsx
@@ -1,17 +1,22 @@
 import Image from 'next/image';
+import formatPrice from '@/utils/format-price';
+import { ProductType } from '@/types/ProductType';
 
-type PageProps = {
-  name: string;
-  image: string;
-  price: number | null;
-};
-
-export default function Product({ name, image, price }: PageProps) {
+export default function Product({ name, image, price }: ProductType) {
   return (
     <div>
-      <Image src={image} alt={name} width={400} height={400} priority={true} />
-      <h1>{name}</h1>
-      <p>{price}</p>
+      <Image
+        src={image}
+        alt={name}
+        width={400}
+        height={400}
+        priority={true}
+        className="h-96 w-full rounded-lg object-cover"
+      />
+      <div className="py-2 font-medium">
+        <h1>{name}</h1>
+        <h2 className="text-primary text-sm">{formatPrice(price)}</h2>
+      </div>
     </div>
   );
 }
